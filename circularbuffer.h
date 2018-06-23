8da5b6c71e6c39346924b33dd9707a88fd1f45b9
PoDuReM <ipoduremennykh@mail.ru>
2018-06-23 19:38:57 +0300

    haha

--- circularbuffer.h
+++ circularbuffer.h
@@ -7,173 +7,328 @@
 
 template<typename T>
 class CircularBuffer {
+public:
+    template<typename U>
+    class Iterator {
+    public:
+        using difference_type = std::ptrdiff_t;
+        using value_type = U;
+        using pointer = U *;
+        using reference = U &;
+        using iterator_category = std::random_access_iterator_tag;
+
+        friend class CircularBuffer;
+
+        template<typename V>
+        Iterator(const Iterator<V> &other);
+
+        Iterator &operator++();
+
+        Iterator operator++(int);
+
+        Iterator &operator--();
+
+        Iterator operator--(int);
+
+        Iterator operator+(ptrdiff_t);
+
+        Iterator operator-(ptrdiff_t);
+
+        Iterator &operator+=(ptrdiff_t);
+
+        Iterator &operator-=(ptrdiff_t);
+
+        U &operator*() const;
+
+        U *operator->() const;
+
+        friend bool operator==(const Iterator &lhs, const Iterator &rhs);
+
+        friend bool operator!=(const Iterator &lhs, const Iterator &rhs);
+
+    private:
+        explicit Iterator(U *, size_t, size_t, size_t);
+
+        U *ptr_;
+        size_t index, beg, capacity;
+    };
+
+    using iterator = Iterator<T>;
+    using const_iterator = Iterator<const T>;
+    using reverse_iterator = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 private:
     T *deque;
     size_t size_, start_, end_, capacity;
 
-    size_t getPrev(size_t i) {
-        if (i == 0) {
-            return capacity - 1;
-        }
-        return i - 1;
-    }
+    size_t getPrev(size_t);
 
-    size_t getNext(size_t i) {
-        if (i == capacity - 1) {
-            return 0;
-        }
-        return i + 1;
-    }
+    size_t getNext(size_t);
 
 public:
-    explicit CircularBuffer(size_t n = 0) {
-        deque = operator new(sizeof(T) * n);
-        capacity = n;
-        start_ = end_ = size_ = 0;
-    }
+    explicit CircularBuffer(size_t = 0);
 
-    CircularBuffer(CircularBuffer const &other) : CircularBuffer(other.size_) {
-        std::copy(other.deque, other.deque + size_, deque);
-    }
+    CircularBuffer(CircularBuffer const &);
 
-    CircularBuffer &operator=(CircularBuffer const &other) {
-        CircularBuffer(other).swap(*this);
-        return *this;
-    }
+    CircularBuffer &operator=(CircularBuffer const &);
 
-    void swap(CircularBuffer &other) {
-        std::swap(deque, other.deque);
-        std::swap(size_, other.size_);
-        std::swap(start_, other.start_);
-        std::swap(end_, other.end_);
-    }
+    void swap(CircularBuffer &);
 
-    ~CircularBuffer() {
-        for (size_t i = 0; i < capacity; ++i) {
-            (deque + i)->~T();
-        }
-        operator delete(deque);
-    };
+    ~CircularBuffer();
 
-    void resize(size_t new_cap) {
-        CircularBuffer newBuf = CircularBuffer(new_cap);
-        for (size_t i = 0; i < size_; ++i) {
-            newBuf.push_back(operator[](i));
-        }
-        swap(newBuf);
-    }
+    void resize(size_t);
 
-    void push_front(T const &a) { //strong
-        if (capacity == size_) {
-            resize(capacity ? capacity * 2 : 1);
-        }
-        deque[getPrev(start_)] = a;
-        start_ = getPrev(start_);
-        ++size_;
-    }
+    void push_front(T const &); //strong
 
-    void push_back(T const &a) { //strong
-        if (capacity == size_) {
-            resize(capacity ? capacity * 2 : 1);
-        }
-        deque[end_] = a;
-        end_ = getNext(end_);
-        ++size_;
-    }
+    void push_back(T const &a); //strong
 
-    void pop_front() noexcept {
-        start_ = getNext(start_);
-        --size_;
-    }
+    void pop_front() noexcept;
 
-    void pop_back() noexcept {
-        end_ = getPrev(end_);
-        --size_;
-    }
+    void pop_back() noexcept;
 
-    void insert(Iterator const &pos_iter, T const &value) { //basic
-        try {
-            size_t pos = pos_iter.getInd();
-            if (capacity == size_) {
-                resize(capacity ? capacity * 2 : 1);
-            }
-            if (pos < size_ - pos) {
-                push_front(front());
-                for (size_t i = 0; i < pos; ++i) {
-                    operator[](i) = operator[](getNext(i));
-                }
-            } else {
-                push_back(back());
-                for (size_t i = size_ - 1; i > pos; --i) {
-                    operator[](i) = operator[](getPrev(i));
-                }
-            }
-            operator[](pos) = value;
-        } catch (...) {}
-    }
+    void insert(iterator &pos_iter, T const &value); //basic
 
-    void erase(Iterator const &pos_iter) { //basic
-        try {
-            size_t pos = pos_iter.getInd();
-            if (pos < size_ - pos) {
-                for (size_t i = pos; i > 0; --i) {
-                    operator[](i) = operator[](getPrev(i));
-                }
-                pop_front();
-            } else {
-                if (size_ > 1) {
-                    for (size_t i = pos; i < size_ - 2; ++i) {
-                        operator[](i) = operator[](getNext(i));
-                    }
-                }
-                pop_back();
-            }
-        } catch (...) {}
-    }
+    void erase(iterator const &pos_iter); //basic
 
-    T &operator[](size_t i) noexcept {
-        if (start_ + i >= capacity)
-            return deque[start_ + i - capacity];
-        return deque[start_ + i];
-    }
+    T &operator[](size_t i) noexcept;
 
-    T operator[](size_t i) const noexcept {
-        if (start_ + i >= capacity)
-            return deque[start_ + i - capacity];
-        return deque[start_ + i];
-    }
+    T operator[](size_t i) const noexcept;
 
-    T front() const noexcept {
-        return operator[](0);
-    }
+    T front() const noexcept;
 
-    T &front() noexcept {
-        return operator[](0);
-    }
+    T &front() noexcept;
 
-    T back() const noexcept {
-        return operator[](size_ - 1);
-    }
+    T back() const noexcept;
 
-    T &back() noexcept {
-        return operator[](size_ - 1);
-    }
+    T &back() noexcept;
 
-    bool empty() const noexcept {
-        return size_ == 0;
-    }
+    bool empty() const noexcept;
 
-    void clear() noexcept {
-        for (size_t i = 0; i < capacity; ++i) {
-            (deque + i)->~T();
-        }
-        operator delete(deque);
-        size_ = capacity = start_ = end_ = 0;
-    }
+    void clear() noexcept;
 };
 
-template <typename T>
-void swap(CircularBuffer<T>& lhs, CircularBuffer<T>& rhs) noexcept {
+template<typename T>
+template<typename U>
+template<typename V>
+CircularBuffer<T>::Iterator<U>::Iterator(const CircularBuffer::Iterator<V> &other) :
+        ptr_(other.ptr_), beg(other.beg), index(other.index), capacity(other.capacity) {}
+
+template<typename T>
+template<typename U>
+CircularBuffer::Iterator &CircularBuffer<T>::Iterator<U>::operator++() {
+    *this += 1;
+    return *this;
+}
+
+template<typename T>
+template<typename U>
+CircularBuffer::Iterator CircularBuffer<T>::Iterator<U>::operator++(int) {
+    Iterator tmp(ptr_);
+    *this += 1;
+    return tmp;
+}
+
+template<typename T>
+template<typename U>
+CircularBuffer::Iterator &CircularBuffer<T>::Iterator<U>::operator--() {
+    *this -= 1;
+    return *this;
+}
+
+template<typename T>
+template<typename U>
+CircularBuffer::Iterator CircularBuffer<T>::Iterator<U>::operator--(int) {
+    Iterator tmp(ptr_);
+    *this -= 1;
+    return tmp;
+}
+
+template<typename T>
+template<typename U>
+CircularBuffer::Iterator CircularBuffer<T>::Iterator<U>::operator+(ptrdiff_t) {
+    Iterator tmp(ptr_,)
+    return tmp;
+}
+
+template<typename T>
+size_t CircularBuffer<T>::getPrev(size_t i) {
+    if (i == 0) {
+        return capacity - 1;
+    }
+    return i - 1;
+}
+
+template<typename T>
+size_t CircularBuffer<T>::getNext(size_t i) {
+    if (i == capacity - 1) {
+        return 0;
+    }
+    return i + 1;
+}
+
+template<typename T>
+CircularBuffer<T>::CircularBuffer(size_t n) {
+    deque = operator new(sizeof(T) * n);
+    capacity = n;
+    start_ = end_ = size_ = 0;
+}
+
+template<typename T>
+CircularBuffer<T>::CircularBuffer(CircularBuffer const &other) : CircularBuffer(other.size_) {
+    std::copy(other.deque, other.deque + size_, deque);
+}
+
+template<typename T>
+CircularBuffer<T> &CircularBuffer<T>::operator=(CircularBuffer const &other) {
+    CircularBuffer(other).swap(*this);
+    return *this;
+}
+
+template<typename T>
+void CircularBuffer<T>::swap(CircularBuffer &other) {
+    std::swap(deque, other.deque);
+    std::swap(size_, other.size_);
+    std::swap(start_, other.start_);
+    std::swap(end_, other.end_);
+}
+
+template<typename T>
+CircularBuffer<T>::~CircularBuffer() {
+    for (size_t i = 0; i < capacity; ++i) {
+        (deque + i)->~T();
+    }
+    operator delete(deque);
+}
+
+template<typename T>
+void CircularBuffer<T>::resize(size_t new_cap) {
+    CircularBuffer newBuf = CircularBuffer(new_cap);
+    for (size_t i = 0; i < size_; ++i) {
+        newBuf.push_back(operator[](i));
+    }
+    swap(newBuf);
+}
+
+template<typename T>
+void CircularBuffer<T>::push_front(const T &a) { //strong
+    if (capacity == size_) {
+        resize(capacity ? capacity * 2 : 1);
+    }
+    deque[getPrev(start_)] = a;
+    start_ = getPrev(start_);
+    ++size_;
+}
+
+template<typename T>
+void CircularBuffer<T>::push_back(const T &a) { //strong
+    if (capacity == size_) {
+        resize(capacity ? capacity * 2 : 1);
+    }
+    deque[end_] = a;
+    end_ = getNext(end_);
+    ++size_;
+}
+
+template<typename T>
+void CircularBuffer<T>::pop_front() noexcept {
+    start_ = getNext(start_);
+    --size_;
+}
+
+template<typename T>
+void CircularBuffer<T>::pop_back() noexcept {
+    end_ = getPrev(end_);
+    --size_;
+}
+
+template<typename T>
+void CircularBuffer<T>::insert(CircularBuffer::iterator &pos_iter, const T &value) { //basic
+    size_t pos = pos_iter - begin();
+    if (capacity == size_) {
+        resize(capacity ? capacity * 2 : 1);
+    }
+    if (pos < size_ - pos) {
+        push_front(front());
+        for (size_t i = 0; i < pos; ++i) {
+            operator[](i) = operator[](getNext(i));
+        }
+    } else {
+        push_back(back());
+        for (size_t i = size_ - 1; i > pos; --i) {
+            operator[](i) = operator[](getPrev(i));
+        }
+    }
+    operator[](pos) = value;
+}
+
+template<typename T>
+void CircularBuffer<T>::erase(const CircularBuffer::iterator &pos_iter) { //basic
+    size_t pos = pos_iter - begin();
+    if (pos < size_ - pos) {
+        for (size_t i = pos; i > 0; --i) {
+            operator[](i) = operator[](getPrev(i));
+        }
+        pop_front();
+    } else {
+        if (size_ > 1) {
+            for (size_t i = pos; i < size_ - 2; ++i) {
+                operator[](i) = operator[](getNext(i));
+            }
+        }
+        pop_back();
+    }
+}
+
+template<typename T>
+T &CircularBuffer<T>::operator[](size_t i) noexcept {
+    if (start_ + i >= capacity)
+        return deque[start_ + i - capacity];
+    return deque[start_ + i];
+}
+
+template<typename T>
+T CircularBuffer<T>::operator[](size_t i) const noexcept {
+    if (start_ + i >= capacity)
+        return deque[start_ + i - capacity];
+    return deque[start_ + i];
+}
+
+template<typename T>
+T CircularBuffer<T>::front() const noexcept {
+    return operator[](0);
+}
+
+template<typename T>
+T &CircularBuffer<T>::front() noexcept {
+    return operator[](0);
+}
+
+template<typename T>
+T CircularBuffer<T>::back() const noexcept {
+    return operator[](size_ - 1);
+}
+
+template<typename T>
+T &CircularBuffer<T>::back() noexcept {
+    return operator[](size_ - 1);
+}
+
+template<typename T>
+bool CircularBuffer<T>::empty() const noexcept {
+    return size_ == 0;
+}
+
+template<typename T>
+void CircularBuffer<T>::clear() noexcept {
+    for (size_t i = 0; i < capacity; ++i) {
+        (deque + i)->~T();
+    }
+    operator delete(deque);
+    size_ = capacity = start_ = end_ = 0;
+}
+
+template<typename T>
+void swap(CircularBuffer<T> &lhs, CircularBuffer<T> &rhs) noexcept {
     lhs.swap(rhs);
 }
 
